87aa7661e35ecd736be714aa949cd7d50d4724c2
Kevin Wong <kkwokwai22@gmail.com>
2017-04-07 16:34:39 -0700

    reverseArray

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,6 +1,20 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
+var loopDown = function(n) {
+  while(n > 0) {
+    console.log(n)
+    n--;
+  }
+}
+
+
 
 //2. Next, try looping just like above except using recursion
+var loopDown = function(n) {
+  if(n === 0) {
+    return n
+  }
+  return n - loopDown(n)
+}
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 
@@ -9,3 +23,15 @@
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+
+var recursiveReverse = function(arr) {
+  var emptyArray = [];
+  debugger;
+  var re = function(arr) {
+    if(arr.length === 0) {
+      return emptyArray;
+    }
+    emptyArray.push(arr.pop());
+   }
+   return re(arr);
+}
--- recursion/reverse.js
+++ recursion/reverse.js
@@ -4,3 +4,19 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+
+var revereseString = function(str) {
+    var emptyString = '';
+    var recursive = function(orderStr) {
+      if(orderStr > 0) {
+        return emptyString
+      }
+      console.log(str.slice(0, -i))
+      emptyString += str.slice(0, -i);
+      console.log(emptyString)
+      count++;
+    }
+    recursive(str)
+}
+
+revereseString("kevin");
